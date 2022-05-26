8bc6aae1f8441b64b800de05be6318b0c2b85f7c
Milena Ramírez López <milena.ramirez.lopez@gmail.com>
2022-05-26 00:18:49 -0400

    JournalApp: validation form user register

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useForm} from '../../hooks/useForm';
+import validator from 'validator';
+import {useDispatch, useSelector} from 'react-redux';
+import {uiReducer} from '../../reducers/uiReducer';
+import {removeError, setError} from '../../actions/ui';
 
 export const RegisterScreen = () => {
+
+    const dispatch = useDispatch();
+    const {msgError} = useSelector(state => state.ui);
+
+    const [formValues, handleInputChange] = useForm({
+        name: 'Milena',
+        email: 'milena.ramirez.lopez@gmail.com',
+        password: '123456',
+        password2: '123456'
+    });
+
+    const {name, email, password, password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if(isFormValid()) {
+            console.log("Formulario correcto");
+        }
+
+    }
+
+    const isFormValid = () => {
+
+        if(name.trim().length === 0) {
+            dispatch( setError('Name is required'));
+            return false;
+        } else if (!validator.isEmail(email)) {
+            dispatch( setError('Email is not valid'));
+            return false;
+        } else if (password !== password2 || password.length < 5) {
+            dispatch( setError('Password should be at least 6 characters'));
+            return false;
+        }
+        dispatch( removeError(''));
+        return true;
+    }
+
     return (
         <>
             <h3 className="auth_title">Register</h3>
 
-            <form>
+            <form onSubmit={handleRegister}>
+                {
+                    msgError &&
+                    (
+                        <div className="auth_alert-error">
+                            {msgError}
+                        </div>
+                    )
+                }
+
 
                 <input
                     type="text"
@@ -14,6 +65,8 @@ export const RegisterScreen = () => {
                     name="name"
                     className="auth_input"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -22,6 +75,8 @@ export const RegisterScreen = () => {
                     name="email"
                     className="auth_input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -29,6 +84,8 @@ export const RegisterScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth_input"
+                    value={password}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -36,6 +93,8 @@ export const RegisterScreen = () => {
                     placeholder="Confirm password"
                     name="password2"
                     className="auth_input"
+                    value={password2}
+                    onChange={handleInputChange}
                 />
 
                 <button
--- 08-journal-app/src/types/types.js
+++ 08-journal-app/src/types/types.js
@@ -3,4 +3,7 @@
 export const  types = {
     login: '[Auth] login',
     logout: '[Auth] logout',
+
+    uiSetError: '[UI] Set error',
+    uiRemoveError: '[UI] Remove error',
 }
